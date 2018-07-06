2050fe2fa72668aab533a6f1c918b57fd49f3ae4
cecrigope <cecrigope@hotmail.com>
2018-07-05 20:33:41 -0400

    Password confirm and match

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -18,4 +18,40 @@ angular.module('userControllers', ['userServices'])
         }
       });
     };
-  });
+  })
+
+  .directive('match', function() {
+    return {
+      restrict: 'A',
+      controller: function($scope) {
+
+        $scope.confirmed = false;
+
+        $scope.doConfirm = function(values) {
+          values.forEach(function(ele) {
+
+            if ($scope.confirm == ele) {
+              $scope.confirmed = true;
+            } else {
+              $scope.confirmed = false;
+            }
+            console.log(ele);
+            console.log($scope.confirm);
+          });
+        }
+      },
+
+      link: function(scope, element, attrs) {
+
+        attrs.$observe('match', function() {
+          scope.matches = JSON.parse(attrs.match);
+          scope.doConfirm(scope.matches);
+        });
+
+        scope.$watch('confirm', function() {
+          scope.matches = JSON.parse(attrs.match);
+          scope.doConfirm(scope.matches);
+        });
+      }
+    };
+  })
