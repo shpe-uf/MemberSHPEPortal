3d9d76e5105bcff748ffc801b15f803c6c4219db
cecrigope <cecrigope@hotmail.com>
2019-06-06 23:00:28 -0500

    Hid edit functionality, added more info functionality for the ompanies in the admin panel

--- public/app/controllers/adminCtrl.js
+++ public/app/controllers/adminCtrl.js
@@ -345,6 +345,39 @@ angular.module('adminController', [])
       });
     };
 
+
+    this.openCompanyInfoModal = function(companyId) {
+      $("#moreInfoModal").modal({
+        backdrop: 'static'
+      });
+
+      User.getCompanyInfo(companyId).then(function(data) {
+        app.company = data.data.message;
+        app.company.majorsList = "";
+        app.company.industryList = "";
+
+        for (var i = 0; i < app.company.majors.length; i++) {
+          if (i === app.company.majors.length - 1) {
+            app.company.majorsList += app.company.majors[i];
+          } else {
+            app.company.majorsList += (app.company.majors[i] + ", ");
+          }
+        }
+
+        for (var i = 0; i < app.company.industry.length; i++) {
+          if (i === app.company.industry.length - 1) {
+            app.company.industryList += app.company.industry[i];
+          } else {
+            app.company.industryList += (app.company.industry[i] + ", ");
+          }
+        }
+      });
+    };
+
+    this.closeCompanyInfoModal = function() {
+      $('#moreInfoModal').modal('hide');
+    };
+
     User.getUsers().then(function(data) {
       if (data.data.success) {
         if (data.data.permission === 'admin') {
